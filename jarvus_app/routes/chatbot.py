d19a52644f5eab8ca7d866c79326a656925223f8
Tetsu Kurumisawa <tetsu.kurumisawa@yale.edu>
2025-06-07 16:23:31 -0400

    Added an "@" to chatbot (w/o backend)

--- jarvus_app/routes/chatbot.py
+++ jarvus_app/routes/chatbot.py
@@ -1,15 +1,25 @@
-# from flask import Blueprint, render_template, request, jsonify, session, redirect, url_for
-# from ..services.mcp_service import mcp_service
+from flask import Blueprint, render_template, request, jsonify, session, redirect, url_for
+from ..services.mcp_service import mcp_service
+from ..models.oauth import OAuthCredentials
+from flask_login import current_user, login_required
 
-# chatbot_bp = Blueprint('chatbot', __name__)
+chatbot_bp = Blueprint('chatbot', __name__)
 
-# @chatbot_bp.route('/')
-# def chat_page():
-#     """Render the chatbot interface."""
-#     user = session.get('user')
-#     if not user:
-#         return redirect(url_for('auth.signin'))
-#     return render_template('chatbot.html', user=user)
+@chatbot_bp.route('/')
+@login_required
+def chat_page():
+    """Render the chatbot interface."""
+    # Check which services are connected
+    gmail_connected = OAuthCredentials.get_credentials(current_user.id, 'gmail') is not None
+    notion_connected = OAuthCredentials.get_credentials(current_user.id, 'notion') is not None
+    slack_connected = OAuthCredentials.get_credentials(current_user.id, 'slack') is not None
+    zoom_connected = OAuthCredentials.get_credentials(current_user.id, 'zoom') is not None
+    
+    return render_template('chatbot.html',
+                         gmail_connected=gmail_connected,
+                         notion_connected=notion_connected,
+                         slack_connected=slack_connected,
+                         zoom_connected=zoom_connected)
 
 # @chatbot_bp.route('/send', methods=['POST'])
 # def send_message():
