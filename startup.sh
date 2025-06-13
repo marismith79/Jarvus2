fbc8ddfb0d5d1f21a17d0647ea81ec732db943b4
shomarismith@icloud.com <shomarismith@icloud.com>
2025-06-12 23:09:02 -0400

    fixing issue maybe

--- startup.sh
+++ startup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Install ODBC Driver
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
+
+# Start the application
 gunicorn --bind=0.0.0.0:$PORT wsgi:app 
