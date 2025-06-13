816e8cc3aa2d95272e5998834682feb95e554f40
shomarismith@icloud.com <shomarismith@icloud.com>
2025-06-12 22:53:36 -0400

    fixing issue maybe

--- jarvus_app/config.py
+++ jarvus_app/config.py
@@ -13,17 +13,20 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-dev-key")
     DEBUG = True
 
-    # Use an absolute path for the database to avoid ambiguity
-    SQLALCHEMY_DATABASE_URI = os.getenv("AZURE_SQL_CONNECTION_STRING")
-    if not SQLALCHEMY_DATABASE_URI:
-        raise ValueError(
-            "AZURE_SQL_CONNECTION_STRING environment variable is not set"
-        )
-
-    # Add timeout parameters to the connection string
-    if "?" in SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI += "&timeout=60&connect_timeout=60"
-    else:
-        SQLALCHEMY_DATABASE_URI += "?timeout=60&connect_timeout=60"
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        db_uri = os.getenv("AZURE_SQL_CONNECTION_STRING")
+        if not db_uri:
+            raise ValueError(
+                "AZURE_SQL_CONNECTION_STRING environment variable is not set"
+            )
+        
+        # Add timeout parameters to the connection string
+        if "?" in db_uri:
+            db_uri += "&timeout=60&connect_timeout=60"
+        else:
+            db_uri += "?timeout=60&connect_timeout=60"
+        
+        return db_uri
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
