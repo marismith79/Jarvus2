b5a81295af3727c03bf8e7f21c111a50896afc94
shomarismith@icloud.com <shomarismith@icloud.com>
2025-06-13 11:24:31 -0400

    fixing config file for dev 2

--- jarvus_app/config.py
+++ jarvus_app/config.py
@@ -20,11 +20,5 @@ class Config:
             "AZURE_SQL_CONNECTION_STRING environment variable is not set"
         )
     
-    # Add timeout parameters to the connection string
-    if "?" in db_uri:
-        db_uri += "&timeout=60&connect_timeout=60"
-    else:
-        db_uri += "?timeout=60&connect_timeout=60"
-    
     SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
